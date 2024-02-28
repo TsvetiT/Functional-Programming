90bd788af96cb31b2463f9322cdbddb68711241d
TsvetiT <tsvetitincheva@gmail.com>
2024-02-28 17:57:13 +0200

    task2

--- task2.hs
+++ task2.hs
@@ -1,1 +1,10 @@
 --Задача 2. Да се дефинира функцията isInside x a b, която проверява дали числото x се намира в затворения интервал [a, b].
+main :: IO()
+main = do
+    print(inSide 2 1 5)
+
+
+inSide:: Int -> Int -> Int -> Bool 
+inSide x a b=
+    if(a<=x && x<=b) then True
+    else False
